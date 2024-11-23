0c9fe28949e7a0c20abec8b462b5b8563ae9f70a
PolySpade <xudonald889@gmail.com>
2024-11-23 11:41:52 +0800

    Add project files.

---
+++ MCO2/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+//extern void imgCvtGrayIntToDouble();
+
+int main(int argc, char* argv[]) {
+	//imgCvtGrayIntToDouble();
+	//input height, width,integer values
+
+
+
+	return 0;
+}
