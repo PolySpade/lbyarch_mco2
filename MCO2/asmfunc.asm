d08c7bb48098ca14f3cd5c5151a8f1fcf41d1a18
PolySpade <xudonald889@gmail.com>
2024-11-24 15:52:40 +0800

    modified logic, input is now int_array instead of double_array.

--- MCO2/asmfunc.asm
+++ MCO2/asmfunc.asm
@@ -1,18 +1,66 @@
 ; assembly part using x86-64
 
 section .data
-msg db "Hello World",13,10,0
+divisor dq 255.0
+
+result dq 0.0
 
 section .text
 bits 64
 default rel ; to handle address relocation
 
-global imgCvtGrayIntToDouble
+global asm_imgCvtGrayIntToDouble
 extern printf
 
-imgCvtGrayIntToDouble:
-	sub rsp, 8*5 ; caller
-	lea rcx, [msg]
-	call printf
-	add rsp, 8*5
-	ret
+asm_imgCvtGrayIntToDouble:
+;rcx - pointer to pointer int array
+;rdx - pointer to pointer doubles array
+;r8d - contains rows [integer]
+;r9d - contains columns [integer]
+;r10 - pointer to current row (int array)
+;r11 - pointer to current row (double array)
+;r12 - pointer to current column (int array)
+;r13 - pointer to current column (double array)
+;r14d - row counter
+;r15d - column counter
+
+    xor r14,r14;column counter
+    xor r15,r15;row counter
+
+    row_loop:
+        mov r10, [rcx + r14 * 8]; rows [integer]
+        mov r11, [rdx + r14 * 8]; rows [double]
+        xor r15, r15
+        ;check if maximum rows reach
+        cmp r14d, r8d
+        je end
+
+        inc r14d
+        jmp col_loop
+
+    col_loop:
+        lea r12, [r10 + r15 * 4]; columns [integer]
+        lea r13, [r11 + r15 * 8]; columns [double]
+
+        ;convert int to double
+        mov eax, [r12]
+        cvtsi2sd xmm0, eax
+        
+        ;divide by 255
+        movsd xmm1, [divisor]
+        VDIVSD xmm3, xmm0, xmm1
+
+        movsd [r13], xmm3; store to double array
+
+        inc r15d
+        ;check if maximum columns reach
+        cmp r15d, r9d
+        je row_loop
+
+
+        jmp col_loop
+
+    end:
+        ret
+
+
--- MCO2/main.c
+++ MCO2/main.c
@@ -1,56 +1,167 @@
+// XU, Donald G.
+// TAN, Richmond T.
+
+#define _CRT_SECURE_NO_WARNINGS //for scanf
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
+#include <time.h>
 
 
+extern void asm_imgCvtGrayIntToDouble(int** int_array, double** double_array, int height, int width);
 
-//extern void imgCvtGrayIntToDouble();
-void c_imgCvtGrayIntToDouble(double** double_array, int height, int width) {
+
+void c_imgCvtGrayIntToDouble(int** int_array, double** c_double_array, int height, int width) {
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
-            double_array[i][j] /= 255.0; //divide
+            c_double_array[i][j] = (double)int_array[i][j] / 255.0;
+        }
+    }
+}
+
+void rand_array(int** int_array, int height, int width) {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            int_array[i][j] = rand() % 256; //random pixel value in range [0, 255]
         }
     }
 }
 
 int main(int argc, char* argv[]) {
+    srand(time(NULL));// for rand
+    //dimensions
     int height, width;
+    double temp;
 
     //get input , w h
     printf("Input: \n");
     scanf("%d %d", &height, &width);
 
     //double float array, pixel values
-    double** double_array = (double**)malloc(height * sizeof(double*));
-    for (int i = 0; i < height; i++) {
-        double_array[i] = (double*)malloc(width * sizeof(double));
-    }
+    int** int_array = (int**)malloc(height * sizeof(int*));
 
-    //read pixel values
+    double** c_double_array = (double**)malloc(height * sizeof(double*));
+    double** asm_double_array = (double**)malloc(height * sizeof(double*));
+    for (int i = 0; i < height; i++) {
+        int_array[i] = (int**)malloc(width * sizeof(int*));
+        c_double_array[i] = (double*)malloc(width * sizeof(double));
+        asm_double_array[i] = (double*)malloc(width * sizeof(double));
+    }
+  
+    //read pixel values and initialize
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
-            scanf("%lf", &double_array[i][j]);
+            scanf("%lf", &temp);
+            //initialize
+            int_array[i][j] = temp;
+            c_double_array[i][j] = temp;
+            asm_double_array[i][j] = temp;
         }
     }
+    //run main functions without time testing
+    c_imgCvtGrayIntToDouble(int_array, c_double_array, height, width);
+    asm_imgCvtGrayIntToDouble(int_array, asm_double_array, height, width);
 
-    c_imgCvtGrayIntToDouble(double_array, height, width);
-
-
-    //print output
+    //print c output
+    printf("-----------------\n");
+    printf("Results in C\n");
     printf("Output:\n");
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
-            printf("%.2f ", double_array[i][j]);
+            printf("%.2f ", c_double_array[i][j]);
+        }
+        printf("\n");
+    }
+
+    //print asm output
+    printf("-----------------\n");
+    printf("Results in ASM\n");
+    printf("Output:\n");
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            printf("%.2f ", asm_double_array[i][j]);
         }
         printf("\n");
     }
 
 
+    //Time test with random values generated
+    //run for 30 times
+    //height: 1000, width: 1000 
+    //Timing for C ; run 30 times
+    //number of tests
+    int tests = 30;
+    //time elapsed
+    double c_elapsed_time = 0, asm_elapsed_time = 0;
+    int tempheight = 1000;
+    int tempwidth = 1000;
+
+    //allocate array
+    int** tempint_array = (int**)malloc(tempheight * sizeof(int*));
+
+    double** tempc_double_array = (double**)malloc(tempheight * sizeof(double*));
+    double** tempasm_double_array = (double**)malloc(tempheight * sizeof(double*));
+    for (int i = 0; i < tempheight; i++) {
+        tempint_array[i] = (int**)malloc(tempwidth * sizeof(int*));
+        tempc_double_array[i] = (double*)malloc(tempwidth * sizeof(double));
+        tempasm_double_array[i] = (double*)malloc(tempwidth * sizeof(double));
+    }
+
+    //random input
+    rand_array(tempint_array, tempheight, tempwidth);
+
+    clock_t c_total_time = 0;
+    for (int t = 0; t < tests; t++) {
+        // Restore the original values
+        for (int i = 0; i < tempheight; i++) {
+            for (int j = 0; j < tempwidth; j++) {
+                tempc_double_array[i][j] = (double)tempint_array[i][j] / 255.0;
+            }
+        }
+        clock_t c_start = clock();
+        c_imgCvtGrayIntToDouble(tempint_array, tempc_double_array, tempheight, tempwidth);
+        clock_t c_end = clock();
+        c_total_time += (c_end - c_start);
+    }
+    c_elapsed_time = ((double)(c_total_time) / CLOCKS_PER_SEC / tests) *1000;
+    // Timing for Assembly ; run 30 times
+    clock_t asm_total_time = 0;
+    for (int t = 0; t < tests; t++) {
+        // Restore the original values
+        for (int i = 0; i < tempheight; i++) {
+            for (int j = 0; j < tempwidth; j++) {
+                tempasm_double_array[i][j] = (double)tempint_array[i][j] / 255.0;
+            }
+        }
+        clock_t asm_start = clock();
+        asm_imgCvtGrayIntToDouble(tempint_array, tempasm_double_array, tempheight, tempwidth);
+        clock_t asm_end = clock();
+        asm_total_time += (asm_end - asm_start);
+    }
+    asm_elapsed_time = ((double)(asm_total_time) / CLOCKS_PER_SEC / tests) *1000;
+
+
+    printf("-----------------\n");
+    printf("Time Elapsed\nAverage time of %d tests\n", tests);
+    printf("Tested with Image Dimensions of\nHeight:%d Width:%d\n", tempheight, tempwidth);
+    printf("C: %f ms\n", c_elapsed_time);
+    printf("ASM: %f ms\n", asm_elapsed_time);
+
 
     //free malloc , end of program
     for (int i = 0; i < height; i++) {
-        free(double_array[i]);
+        free(int_array[i]);
+        free(c_double_array[i]);
+        free(asm_double_array[i]);
+        free(tempint_array[i]);
+        free(tempc_double_array[i]);
+        free(tempasm_double_array[i]);
     }
-    free(double_array);
+    free(c_double_array);
+    free(asm_double_array);
+    free(int_array);
+    free(tempc_double_array);
+    free(tempasm_double_array);
+    free(tempint_array);
     return 0;
+ 
 }
